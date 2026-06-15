e2ff0ec9c0975b982ed59c0d4baa11566664a001
artthejim-web <mbuguajim59@gmail.com>
2026-06-15 13:29:27 +0300

    Add pricing in Kenyan Shillings to gallery items

--- script.js
+++ script.js
@@ -4,78 +4,91 @@ const galleryItems = [
         title: "Eminem",
         medium: "Charcoal & Graphite",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork1.jpg.jpg"
     },
     {
         title: "SHE",
         medium: "Charcoal, Graphite & Colored Pencils",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork2.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Colored Pencils",
         size: "A4 (210 x 297 mm)",
+        price: "KSh. 3,000",
         image: "artwork3.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Colored Pencils",
         size: "A5 (200 x 200 mm)",
+        price: "KSh. 3,000",
         image: "artwork4.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal & Graphite",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork5.jpg.jpg"
     },
     {
         title: "Eccedentesiast",
         medium: "Charcoal, Graphite & Pastels",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork6.jpg.jpg"
     },
     {
         title: "Bloom in Silence",
         medium: "Charcoal, Graphite & Colored Pencils",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork7.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal & Graphite",
         size: "A4 (210 x 297 mm)",
+        price: "KSh. 3,000",
         image: "artwork8.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal & Pastels",
         size: "A4 (210 x 297 mm)",
+        price: "KSh. 3,000",
         image: "artwork9.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal, Pastels & Watercolors",
         size: "A2 (500 x 600 mm)",
+        price: "KSh. 15,000",
         image: "artwork10.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal & Pastels",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork11.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Charcoal & Pastels",
         size: "A4 (210 x 297 mm)",
+        price: "KSh. 3,000",
         image: "artwork12.jpg.jpg"
     },
     {
         title: "Untitled",
         medium: "Mixed Media",
         size: "A3 (297 x 420 mm)",
+        price: "KSh. 5,600",
         image: "artwork13.jpg.jpg"
     }
 ];
@@ -97,8 +110,9 @@ function loadGallery() {
                 <div class="gallery-item-details">
                     <p><strong>Medium:</strong> ${item.medium}</p>
                     <p><strong>Size:</strong> ${item.size}</p>
+                    <p class="price"><strong>Price:</strong> ${item.price}</p>
                 </div>
-                <a href="https://wa.me/254103142621?text=Hi%20Jim%2C%20I%27m%20interested%20in%20%22${encodeURIComponent(item.title)}%22" target="_blank" class="order-button">
+                <a href="https://wa.me/254103142621?text=Hi%20Jim%2C%20I%27m%20interested%20in%20%22${encodeURIComponent(item.title)}%22%20-%20${item.price}" target="_blank" class="order-button">
                     <i class="fab fa-whatsapp"></i> Order via WhatsApp
                 </a>
             </div>
